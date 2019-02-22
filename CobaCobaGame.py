2b41bbd6a93c59936a46216f86eee4713452f943
muchammadibnu <47872314+muchammadibnu@users.noreply.github.com>
2019-02-22 19:31:31 +0700

    Update CobaCobaGame.py

--- CobaCobaGame.py
+++ CobaCobaGame.py
@@ -1,58 +1,58 @@
 import sys, math, random
 
-Zona_Platinum=[]
-Total_Platinum=0
-Range={}
+Zones_Platinum=[]
+total_Platinum=0
+chance={}
 
-playerCount, my_id, zone_count, link_count = [int(i) for i in input().split()]
+player_count, my_id, zone_count, link_count = [int(i) for i in input().split()]
 #Read Platinum in Zone
 for i in range(zone_count):
    zone_id, platinum_source = [int(i) for i in input().split()]
-   Zona_Platinum.append(platinum_source)
+   Zones_Platinum.append(platinum_source)
    
 for i in range(link_count):
    zone1, zone2 = [int(i) for i in input().split()]
-   if zone1 not in Range:
-       Range[zone1]=[]
-   if zone2 not in Range:
-       Range[zone2]=[]
-   if zone2 not in Range[zone1]:
-       Range[zone1].append(zone2)
-   if zone1 not in Range[zone2]:
-       Range[zone2].append(zone1)
+   if zone1 not in chance:
+       chance[zone1]=[]
+   if zone2 not in chance:
+       chance[zone2]=[]
+   if zone2 not in chance[zone1]:
+       chance[zone1].append(zone2)
+   if zone1 not in chance[zone2]:
+       chance[zone2].append(zone1)
 
 # game loop
 while True:
    Zones=[]
-   zones_own=[]
-   My_Zones=[]
+   zone_present=[]
+   my_zones=[]
    spawn=""
-   Perpindahan=""
+   perpindahan=""
    
    platinum = int(input()) # my available Platinum
    #Read My Places
    for i in range(zone_count):
-       z_id, owner_id, pods_P0, pods_P1, podsP2, podsP3 = [int(i) for i in input().split()]
+       zId, ownerId, podsP0, pods_P1, podsP2, podsP3 = [int(i) for i in input().split()]
        exec("count=podsP"+str(my_id))
-       Zones.append(owner_id)
+       Zones.append(ownerId)
        if count:
-           zones_own.append([z_id,owner_id,count])
+           zone_present.append([zId,ownerId,count])
    
    #moving
-   for zone in zones_own:
+   for zone in zone_present:
        if zone[1]==my_id:
-           pilihan=random.randint(0,len(Range[zone[0]])-1)
-           Perpindahan+=str(zone[2])+" "+str(zone[0])+" "+str((Range[zone[0]])[pilihan])+" "
+           probability=random.randint(0,len(chance[zone[0]])-1)
+           perpindahan+=str(zone[2])+" "+str(zone[0])+" "+str((chance[zone[0]])[probability])+" "
    
    for zone in range(len(Zones)-1):
        if Zones[zone]==my_id:
-           Total_Platinum+=Zona_Platinum[zone]
-           My_Zones.append(zone)
-   while Total_Platinum>=20:
-       pilihan=random.randint(0,len(My_Zones)-1)
-       spawn+="1 "+str(My_Zones[pilihan])+" "
-       Total_Platinum-=20
+           total_Platinum+=Zones_Platinum[zone]
+           my_zones.append(zone)
+   while total_Platinum>=20:
+       probability=random.randint(0,len(my_zones)-1)
+       spawn+="1 "+str(my_zones[probability])+" "
+       total_Platinum-=20
    
    #Instructions
-   print(Perpindahan) 
+   print(perpindahan) 
    print(spawn)
